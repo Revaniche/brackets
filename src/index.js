c517b4546263ae36c865d6582f8cee6726a4024f
Revaniche <revaniche@gmail.com>
2019-10-09 21:34:16 +0400

    create code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    var op = [];
+    var level = 0;
+    var a=str;
+   
+    for (var i = 0; i < a.length; i++) {
+        for (var j=0; j < bracketsConfig.length; j++) {
+            if (a[i]==bracketsConfig[j][1]) {      
+                if (op[level]==j) {        
+                    level--
+                } else {
+                    if (a[i]==bracketsConfig[j][0]) {     
+                        level++;
+                        op[level] = j
+                    } else {    
+                         return false       
+                    }
+                }
+            } else {
+                if (a[i]==bracketsConfig[j][0]) {       
+                    level++;
+                    op[level] = j
+                }
+            }
+        }
+    }
+    return (level==0);
 }
